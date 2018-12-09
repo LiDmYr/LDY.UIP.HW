b45e5c6472098575b3949141a9b51e2967067d73
LDY <liashkod@gmail.com>
2018-12-09 19:16:21 +0200

    Initial commit

---
+++ LDY.UIP.HW1.AppleShop/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LDY.UIP.HW1.AppleShop {
+    class Program {
+        static void Main(string[] args) {
+            int a = 10;
+        }
+    }
+}
